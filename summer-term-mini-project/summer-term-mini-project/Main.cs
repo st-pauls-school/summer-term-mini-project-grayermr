969f6da83adf75828d6002707e9af17800da6776
Max Grayer <maxgrayer@Maxs-MacBook-Pro-3.local>
2018-06-19 12:07:53 +0100

    Project

---
+++ summer-term-mini-project/summer-term-mini-project/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace summertermminiproject
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
