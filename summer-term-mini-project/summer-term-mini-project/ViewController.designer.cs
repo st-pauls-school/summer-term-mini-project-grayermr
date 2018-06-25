a1cf3ac31a51f887a7b79c10d9df504e975f90fe
Max Grayer <maxgrayer@Maxs-MacBook-Pro-3.local>
2018-06-25 09:24:51 +0100

    Project

--- summer-term-mini-project/summer-term-mini-project/ViewController.designer.cs
+++ summer-term-mini-project/summer-term-mini-project/ViewController.designer.cs
@@ -1,18 +1,31 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
+// This file has been generated automatically by Visual Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace summertermminiproject
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Action ("createSale:")]
+		partial void createSale (AppKit.NSButton sender);
+
+		[Action ("newInventory:")]
+		partial void newInventory (AppKit.NSButton sender);
+
+		[Action ("salesReport:")]
+		partial void salesReport (AppKit.NSButton sender);
+
+		[Action ("viewInventory:")]
+		partial void viewInventory (AppKit.NSButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
 }
